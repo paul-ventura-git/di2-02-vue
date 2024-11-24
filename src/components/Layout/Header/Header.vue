7b964876a29c87dde01fe45f688de1f1bbc72076
paul-ventura-git <j.paul.ventura@gmail.com>
2024-11-23 20:50:05 -0500

    Home

--- src/components/Layout/Header/Header.vue
+++ src/components/Layout/Header/Header.vue
@@ -24,7 +24,7 @@ export default {
     <div class="container d-flex flex-wrap justify-content-center">
       <a href="/" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
         <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-        <span class="fs-4">Double header</span>
+        <span class="fs-4">Here it goes the name of your company</span>
       </a>
       <form class="col-12 col-lg-auto mb-3 mb-lg-0" role="search">
         <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
